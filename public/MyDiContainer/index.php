9db11b8d8bcf0a1907209d9132920c28cd899406
msfukui <msfukui@gmail.com>
2023-09-17 23:43:15 +0900

    nyholm/psr7 を入れてユニットテストに追加

--- public/MyDiContainer/index.php
+++ public/MyDiContainer/index.php
@@ -3,13 +3,16 @@
 declare (strict_types=1);
 
 use LearnModernPhp\MyDiContainer\Container;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $container = new Container([
-    'birthday' => '1990-01-01'
+    'birthday' => '1990-01-01',
+    Psr17Factory::class => new \Nyholm\Psr7\Factory\Psr17Factory(),
 ]);
 
 assert($container->get('birthday') === '1990-01-01');
+assert($container->get(Psr17Factory::class) instanceof Psr17Factory);
 
 echo "Ok.¥n";
--- tests/MyDiContainer/ContainerTest.php
+++ tests/MyDiContainer/ContainerTest.php
@@ -4,33 +4,67 @@ declare (strict_types=1);
 
 namespace LearnModernPhp\MyDiContainer;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class ContainerTest extends TestCase
 {
     private Container $container;
 
-    protected function setUp(): void
+    public static function ContainerGetOkProvider(): array
+    {
+        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+        return [
+            'String OK' => [['birthday' => '1990-01-01'], 'birthday', '1990-01-01'],
+            'Psr17Factory OK' => [[Psr17Factory::class => $psr17Factory],
+                Psr17Factory::class, $psr17Factory],
+        ];
+    }
+
+    #[DataProvider('ContainerGetOkProvider')]
+    public function testGetOk(array $data, string $id, mixed $expected): void
+    {
+        $this->container = new Container($data);
+
+        $this->assertSame($expected, $this->container->get($id));
+    }
+
+    public function testGetThrowsException(): void
     {
         $this->container = new Container(['birthday' => '1990-01-01']);
-    }
 
-    public function testGet()
-    {
-        $this->assertSame('1990-01-01', $this->container->get('birthday'));
-    }
-
-    public function testGetThrowsException()
-    {
         $this->expectException(NotFoundException::class);
         $this->expectExceptionMessage("Entry 'anniversary' not found");
 
         $this->container->get('anniversary');
     }
 
-    public function testHas()
+    public static function ContainerHasOkProvider(): array
     {
-        $this->assertTrue($this->container->has('birthday'));
+        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+        return [
+            'String OK' => [['birthday' => '1990-01-01'], 'birthday'],
+            'Psr17Factory OK' => [[Psr17Factory::class => $psr17Factory],
+                Psr17Factory::class],
+        ];
+    }
+
+    #[DataProvider('ContainerHasOkProvider')]
+    public function testHasOk(array $data, string $id): void
+    {
+        $this->container = new Container($data);
+
+        $this->assertTrue($this->container->has($id));
+    }
+
+    public function testHasNg(): void
+    {
+        $this->container = new Container(['birthday' => '1990-01-01']);
+
         $this->assertFalse($this->container->has('anniversary'));
     }
 }
